999d431953c4b3a773ee514fb6f5fd13a117efe7
Hiroki Kajiwara <rerun.0510@gmail.com>
2022-11-14 11:52:09 +0900

    型定義のエラー修正

--- src/libraries/tetris/utils/createNextMinos.ts
+++ src/libraries/tetris/utils/createNextMinos.ts
@@ -5,6 +5,6 @@ import { shuffleArray } from '.'
 /** ７種類のミノから並び順がランダムな配列を作成 */
 export const createNextMinos = (): Mino[] => [
   ...shuffleArray(
-    Object.keys(minos).filter((v) => v !== 'none')
+    Object.keys(minos).filter((v) => v !== 'none') as Mino[]
   ),
 ]
